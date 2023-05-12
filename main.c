c0635ff2860300de6ee4e7ae22101bb83cd40810
N4RMA <manar-yacine@outlook.com>
2023-05-11 22:37:17 -0400

    Added prepare_statement function

--- main.c
+++ main.c
@@ -24,12 +24,37 @@ typedef enum {
 } PrepareResult;
 
 
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
+}
+
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNCRECOGNIZED_STATEMENT;
 }
 
 
