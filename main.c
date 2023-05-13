7470d5b7a220fb098973540348f4989327936079
N4RMA <manar-yacine@outlook.com>
2023-05-13 15:08:30 -0400

    Added Row struct

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <complex.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 
 typedef struct {
