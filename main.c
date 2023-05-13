87ac6f4e510552c2131ec54b3533c72c6ed5a1c8
N4RMA <manar-yacine@outlook.com>
2023-05-13 17:01:42 -0400

    Added row serialization/deserialization functions

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <complex.h>
+#include <stdatomic.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/types.h>
 
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)NULL)->Attribute)
 
 
 typedef struct {
@@ -42,7 +46,18 @@ typedef enum {
 
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
+
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
 
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
@@ -83,6 +98,20 @@ void execute_statement(Statement* statement) {
             puts("This is where we would do a select.");
             break;
     }
+}
+
+
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+
+void deserialization_row(void* source, Row* destination) {
+   memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE); 
+   memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE); 
+   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE); 
 }
 
 
