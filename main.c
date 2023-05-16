7f7e15135da34eda35a9f112fc751322ef844341
N4RMA <manar-yacine@outlook.com>
2023-05-15 23:05:08 -0400

    Added row_slot function

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <complex.h>
-#include <stdatomic.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -125,6 +123,20 @@ void deserialization_row(void* source, Row* destination) {
    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE); 
    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE); 
    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE); 
+}
+
+
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        // Allocate memory only when we try to access the page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    
+    return page + byte_offset;
 }
 
 
