475152e8d4ac0908cc2e8f07367d36a7e83aa459
N4RMA <manar-yacine@outlook.com>
2023-05-16 22:03:24 -0400

    Modified main function

--- main.c
+++ main.c
@@ -40,8 +40,15 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNCRECOGNIZED_STATEMENT
 } PrepareResult;
+
+
+typedef enum { 
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
 
 
 typedef enum {
@@ -70,20 +77,11 @@ const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        exit(EXIT_SUCCESS);
-    } else {
-        return META_COMMAND_UNRECOGNIZED_COMMAND;
-    }
-}
-
-
 // prepares the statement based on the user input
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement.row_to_insert.email);
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
 
         if (args_assigned < 3) {
             return PREPARE_SYNTAX_ERROR;
@@ -151,9 +149,14 @@ void* row_slot(Table* table, uint32_t row_num) {
 }
 
 
+void print_row(Row* row) {
+    printf("%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+
 ExecuteResult execute_insert(Statement* statement, Table* table) {
     if (table->num_rows >= TABLE_MAX_ROWS) {
-        return EXECUTE_TALBE_FULL;
+        return EXECUTE_TABLE_FULL;
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
@@ -165,7 +168,7 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 }
 
 
-ExecuteResult execute_select(Statement statement, Table* table) {
+ExecuteResult execute_select(Statement* statement, Table* table) {
     Row row;
     for (uint32_t i = 0; i < table->num_rows; i++) {
         deserialize_row(row_slot(table, i), &row);
@@ -228,31 +231,55 @@ void close_input_buffer(InputBuffer* input_buffer) {
 }
 
 
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        free_table(table);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+
 // main function
 int main(int argc, char* argv[]) {
     Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
+
     while (true) {
         print_prompt();
-        switch (prepare_statement(input_buffer, &statement)) {
-            case (PREPARE_SUCCESS):
-                break;
-            case (PREPARE_SYNTAX_ERROR):
-                puts("Syntax error. Could not parse statement.");
-                break;
-            case (PREPARE_UNCRECOGNIZED_STATEMENT):
-                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
-                break;
-            continue;
+        read_input(input_buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer, table)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
         }
 
-        switch (execute_statement(&statement, table)) {
-            case (EXECUTE_SUCCESS):
-                puts("EXECUTE_SUCCESS");
-                break;
-            case (EXECUTE_TALBE_FULL):
-                puts("Error: Table full.");
-                break;
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_SYNTAX_ERROR):
+            printf("Syntax error. Could not parse statement.\n");
+            continue;
+        case (PREPARE_UNCRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s'.\n",
+            input_buffer->buffer);
+            continue;
+        }
+    switch (execute_statement(&statement, table)) {
+    	case (EXECUTE_SUCCESS):
+            printf("Executed.\n");
+            break;
+        case (EXECUTE_TABLE_FULL):
+            printf("Error: Table full.\n");
+            break;
         }
     }
 }
